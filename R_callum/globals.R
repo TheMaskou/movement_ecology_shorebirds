f87e03b43a80ef04dd44c08d24539c025db66772
CallumGrape <callum06@gmail.com>
2026-03-31 03:16:03 +0000

    New R script to define globals

---
+++ R_callum/globals.R
@@ -0,0 +1,8 @@
+# ==== Directories and File Paths ====
+dir_motus <- here::here("data", "motus")
+dir_tides <- here::here("data", "tides")
+
+# ==== Motus Tags ====
+motus_tags_test <- c(
+  "43291"
+)
