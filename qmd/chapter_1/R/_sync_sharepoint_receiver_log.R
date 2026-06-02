8524f656e564f8ebadbea806e81cde6b85c0edc2
CallumGrape <callum06@gmail.com>
2026-06-02 19:37:15 +1000

    Update to use the **project**-level .Renviron (otherwise it wasn't working)

--- qmd/chapter_1/R/_sync_sharepoint_receiver_log.R
+++ qmd/chapter_1/R/_sync_sharepoint_receiver_log.R
@@ -3,14 +3,16 @@
 # Survey123) from the SharePoint, into the project data folder.
 
 # To use this, you must first define the path to the SharePoint on your computer
-# as a user-level environment variable. Basically, this allows anyone to run
-# this script (even on different computers with different SharePoint locations)
+# as a project-level environment variable. Basically, this allows anyone to run
+# this script (even on different computers with different SharePoint locations).
+
+# Only works because .Renviron has been added to .gitignore.
 
 # ==== How To Set Up ====
 # This only need to be done once (per computer).
 
-# 1. run the following command in console to open your user-level .Renviron
-# "usethis::edit_r_environ("user")
+# 1. run the following command in console to open your project-level .Renviron
+# "usethis::edit_r_environ("project")
 
 # 2. Add a new environment variable called SHOREBIRD_SHAREPOINT_PATH, 
 # assigned to the full path to the SharePoint on your computer. 
@@ -18,9 +20,12 @@
 
 # SHOREBIRD_SHAREPOINT_PATH = "C:\Users\callu\OneDrive - Student\The University of Newcastle\StudentGroupPhD - Louise Williams and Mattea Taylor - General"
 
-# 3. Either restart R session, or run readRenviron("~/.Renviron")
+# 3. Restart R session so the Renviron is reloaded
 
-# ==== Copy the Files From SharePoint ====
+# ==== Load Packages ====
+source(here::here("qmd", "chapter_1", "R", "globals.R"))
+
+# ==== Define File Paths ====
 
 # !!!! DEFINE THE FILE NAMES OF THE SPREADSHEETS AS THEY ARE IN SHAREPOINT
 # HERE !!!!
@@ -30,6 +35,8 @@ sharepoint_receiver_log_survey123 <- "arcgis-123survey_output_table_raw-manually
 
 path_sharepoint_receiver_log_historic <- here::here(dir_sharepoint, "Motus_array_maintenance", sharepoint_receiver_log_historic)
 path_sharepoint_receiver_log_survey123 <- here::here(dir_sharepoint, "Motus_array_maintenance", sharepoint_receiver_log_survey123)
+
+# ==== Copy Files From SharePoint ====
 
 # Copy the files - note that regardless of the name in the sharepoint, they
 # will be renamed to whatever is defined in globals.R.
